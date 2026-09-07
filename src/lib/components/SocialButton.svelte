731132a4bbfb9ab74505f6d59d73dce63a919109
wooningeire <22846982+wooningeire@users.noreply.github.com>
2026-09-07 00:01:25 -0700

    feat: use vaie std button style

--- src/lib/components/SocialButton.svelte
+++ src/lib/components/SocialButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { Snippet } from "svelte";
+    import Button from "./generic/Button.svelte";
 
 let {
     href,
@@ -17,38 +18,37 @@ let {
     target="_blank"
     rel="noopener noreferrer"
 >
-    <button tabindex="-1">
-        {#if icon !== null}
-            <social-button-icon>{@render icon?.()}</social-button-icon>
-        {/if}
+    <Button>
+        <social-button-display>
+            {#if icon !== null}
+                <social-button-icon>{@render icon?.()}</social-button-icon>
+            {/if}
 
-        <social-button-label>{label}</social-button-label>
-    </button>
+            <social-button-label>{label}</social-button-label>
+        </social-button-display>
+    </Button>
 </a>
 
 <style lang="scss">
-@use "$/styles/mixins";
 
 a {
     display: grid;
     place-items: stretch;
 }
 
-button {
-    @include mixins.glass-button-small;
-
+social-button-display {
     display: flex;
     gap: 0.5em;
     align-items: center;
 
-    > social-button-icon {
-        width: 1.25em;
-        height: 1.25em;
+}
+social-button-icon {
+    width: 1.25em;
+    height: 1.25em;
 
-        display: grid;
-        place-items: center;
-    }
-
+    display: grid;
+    place-items: center;
+    
     :global(svg) {
         width: 100%;
         height: 100%;
---
+++ src/lib/components/generic/Button.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+import type { HTMLButtonAttributes } from "svelte/elements";
+
+let {
+    children,
+    keyHeld = false,
+    displayClass,
+    buttonStyle = "text",
+    red = false,
+    ...buttonProps
+}: {
+    children: Snippet,
+    keyHeld?: boolean,
+    buttonStyle?: "text" | "image" | "icon",
+    displayClass?: string,
+    red?: boolean,
+} & HTMLButtonAttributes = $props();
+</script>
+
+<button {...buttonProps}>
+    <button-display
+        class:key-held={keyHeld}
+        class:text-button={buttonStyle === "text"}
+        class:icon-button={buttonStyle === "icon"}
+        class:red
+        class={displayClass}
+    >
+        {@render children?.()}
+    </button-display>
+</button>
+
+<style lang="scss">
+@use "$/styles/colors.scss";
+@use "$/styles/fonts.scss";
+
+$bg-col: oklch(from colors.$fg l c h / 0.125);
+$bg-stripe-col: oklch(from colors.$fg l c h / 0.25);
+$inset-box-shadow: 0 1em 2em oklch(from colors.$fg l c h / 0.0625) inset;
+$outset-box-shadow: 0 0.25rem 1rem 0.5rem oklch(0 0 0 / 0.125);
+
+
+button {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    place-items: stretch;
+
+    border: none;
+    background: none;
+
+    cursor: pointer;
+
+    font-family: fonts.$font-title;
+
+
+    --bg-col: #{$bg-col};
+
+    
+    &[disabled] {
+        pointer-events: none;
+        opacity: 0.3;
+    }
+
+    button-display {
+        display: block;
+
+        border-radius: 1rem;
+        background: var(--bg-col);
+        box-shadow:
+            $outset-box-shadow,
+            $inset-box-shadow;
+
+        pointer-events: none;
+        overflow: hidden;
+
+        backdrop-filter: blur(4px);
+
+        transition:
+            transform 0.25s cubic-bezier(0,2.75,.47,1),
+            box-shadow 0.1s cubic-bezier(0,1,.47,1),
+            filter 0.25s ease;
+
+        &.red {
+            --bg-col: oklch(0.99 0.15 350 / 0.75);
+        }
+
+        &.text-button {
+            padding: 0.25rem 1rem;
+        }
+
+        &.icon-button {
+            padding: 0.5rem;
+        }
+    }
+
+    &:is(:hover, :focus-visible) > button-display,
+    button-display.key-held {
+        border-color: currentcolor;
+
+        transform: translateY(-0.125rem) scale(1.05);
+
+        animation: sliding-background 1s infinite linear;
+        background-image: repeating-linear-gradient(
+            135deg,
+            $bg-col 0,
+            $bg-col 1rem,
+            $bg-stripe-col 1rem,
+            $bg-stripe-col 2rem,
+        );
+        background-size: calc(100% + 3rem) calc(100% + 3rem);
+
+        @keyframes sliding-background {
+            from {
+                background-position: -2.828427rem -2.828427rem; // 2 * sqrt(2)
+            }
+            to {
+                background-position: 0 0;
+            }
+        }
+    }
+
+    &:active > button-display,
+    button-display.key-held {
+        transform: translateY(0.0625rem) scale(0.95);
+    }
+}
+</style>
